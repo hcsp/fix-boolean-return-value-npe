109930910e2df29d4bef9397f8dbfbaf7d3feda6
hanchenhao <hanchenhao@vip.qq.com>
2020-05-16 18:46:25 +0800

    boolean (#211)
    boolean (#211)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -12,8 +12,8 @@ public class Light {
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
-    public boolean isOn() {
-        return on;
+    public Boolean isOn() {
+        return on == null ? false : on;
     }
 
     public Boolean isOnRawValue() {
