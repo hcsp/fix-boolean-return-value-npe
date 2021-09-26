100e390f9a651a79a6c77444f880b48898356854
AdFeng <3200185362@qq.com>
2021-09-26 14:32:33 +0800

    修复空指针异常

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -12,7 +12,7 @@ public class Light {
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
-    public Boolean isOn() {
+    public boolean isOn() {
         if (on == null) {
             on = false;
         }
