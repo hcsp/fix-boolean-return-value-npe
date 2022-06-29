60db8a16c2e077291fd119bb07f690f4e3d6e7d1
liuyidiao <1070692044@qq.com>
2022-06-29 22:24:04 +0800

    判断

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -12,8 +12,8 @@ public class Light {
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
-    public Boolean isOn() {
-        if(on ==null){
+    public boolean  isOn() {
+        if(on == null){
             return false;
         }
         return on;
