7a25533d4cf037dce52ca2eeadebb950d39ce438
yang.he <yang.he@homepartners.tech>
2022-10-10 22:05:30 +0800

    - [x] return value npe done

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -4,15 +4,15 @@ public class Main {
     // 修复com/github/hcsp/datatype/Light.java，使得运行这个方法不会抛出空指针异常
     public static void main(String[] args) {
         Light light1 = new Light(null);
-        Boolean on1 = light1.isOn();
+        boolean on1 = light1.isOn();
         System.out.println("状态未知的灯亮着吗：" + on1);
 
         Light light2 = new Light(true);
-        Boolean on2 = light2.isOn();
+        boolean on2 = light2.isOn();
         System.out.println("亮着的灯亮着吗：" + on2);
 
         Light light3 = new Light(false);
-        Boolean on3 = light3.isOn();
+        boolean on3 = light3.isOn();
         System.out.println("灭了的灯亮着吗：" + on3);
     }
 }
