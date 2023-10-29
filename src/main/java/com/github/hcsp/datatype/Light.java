070f5a634eaa837d75f009fe60b6b7a4d7c1a2c6
lyraaaaaaa <Lyraaaaaaaaaaa@gmail.com>
2023-10-29 22:18:18 +0800

    9

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -1,22 +1,26 @@
 package com.github.hcsp.datatype;
 
-public class Light {
+import java.util.Objects;
+
+public class Light {         //light类
     // 一盏灯可能有三种状态：
     // 亮 -> true
     // 灭 -> off
     // 未知 -> null
-    Boolean on;
+    Boolean on;      //light的成员变量on是布尔值类型
 
-    public Light(Boolean on) {
+    public Light(Boolean on) {     //构造器
         this.on = on;
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        return Boolean.TRUE.equals(on);
     }
 
+
     public Boolean isOnRawValue() {
-        return on;
+
+        return null;
     }
 }
